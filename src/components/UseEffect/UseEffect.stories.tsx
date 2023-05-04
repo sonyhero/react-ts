909b80d3a6d7395c64d0510f15a6b94681acd173
sonyhero <27anton06@gmail.com>
2023-05-04 16:15:56 +0300

    lesson22-useEffect-setTimeout-setInterval

--- src/components/UseEffect/UseEffect.stories.tsx
+++ src/components/UseEffect/UseEffect.stories.tsx
@@ -10,7 +10,7 @@ const generateNumber = () => {
     return 1
 }
 
-export const UseEffectExample = () => {
+export const SimpleExample = () => {
     const [count, setCount] = useState<number>(generateNumber)
     const [fakeCount, setFakeCount] = useState<number>(generateNumber)
 
@@ -39,4 +39,66 @@ export const UseEffectExample = () => {
             </div>
 
         </>)
+}
+
+export const SetTimeoutExample = () => {
+    const [count, setCount] = useState<number>(generateNumber)
+    const [fakeCount, setFakeCount] = useState<number>(generateNumber)
+
+    useEffect(() => {
+        setTimeout(() => {
+            console.log('setTimeout')
+            document.title = count.toString()
+        }, 1500)
+    })
+
+    return (
+        <div>
+            <div>
+                <button onClick={() => setCount(state => state + 1)}>
+                    increment
+                </button>
+                {count}
+            </div>
+            <div>
+                <button onClick={() => setFakeCount(state => state + 1)}>
+                    fake increment
+                </button>
+                {fakeCount}
+            </div>
+
+        </div>)
+}
+export const SetIntervalExample = () => {
+    const [count, setCount] = useState<number>(generateNumber)
+    const [fakeCount, setFakeCount] = useState<number>(generateNumber)
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log('set interval')
+            setCount(state => state + 1)
+        }, 1000)
+    },[])
+
+    return (
+        <div>
+            <div>
+                Hello, this is count: {count}, and this is fakeCount: {fakeCount}
+            </div>
+        </div>)
+}
+export const ClockExample = () => {
+    const [date, setDate] = useState(new Date())
+
+    useEffect(() => {
+        setInterval(() => {
+            console.log('set interval')
+            setDate(state => new Date())
+        }, 1000)
+    },[])
+
+    return (
+        <div>
+            Hello, this is clock: {`${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`}
+        </div>)
 }
